db3ea34fd3d54709a380752bd01c5e8b7859b644
Shin <sin1315a@naver.com>
2024-02-08 13:30:09 +0900

    set

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/config/configStore.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </Provider>
 );
--- src/pages/shared/Router.jsx
+++ src/pages/shared/Router.jsx
@@ -1,11 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "../Home";
+import Home from "../Home.jsx";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
