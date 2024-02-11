c9dc3ab4c504edd2ec4da9cf289c8a87bf9da2c4
gkstnadl <gkstnadl12@naver.com>
2024-02-12 07:01:11 +0900

    Docs : Router 위에 Header 고정시켜둠

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -3,10 +3,12 @@ import Home from "../pages/Home";
 import WritePost from "../components/write/WritePost";
 import Mypage from "../../src/pages/Mypage";
 import Login from "../components/layout/Login";
+import Header from "components/layout/Header";
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/write" element={<WritePost />} />
