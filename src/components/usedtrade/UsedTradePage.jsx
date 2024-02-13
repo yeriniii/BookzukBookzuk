d5b9757f08200528c04ddccb925b3f283d4d5c01
Your Name <you@your-domain.com>
2024-02-13 12:06:11 +0900

    docs: 필요없는 문서 삭제

--- src/components/usedtrade/UsedTradePage.jsx
+++ src/components/usedtrade/UsedTradePage.jsx
@@ -7,7 +7,6 @@ import BookCards from "../main/BookCards";
 function UsedTradePage() {
   //리뷰리스트
   const lists = useSelector((state) => state.list);
-  console.log(lists);
 
   // 검색기능
   const [searchText, setSearchText] = useState("");
@@ -37,14 +36,6 @@ function UsedTradePage() {
           <button onClick={searchButton}>검색</button>
         </SearchBox>
         <BookList>
-          {/* {가져온데이터 === "리뷰"
-          ? lists.map((list) => {
-              if (list.글종류 === "리뷰" || list.글종류 === "추천") {
-                return <BookCards key={list.id} list={list} />;
-              }}) :
-              lists.filter((list) => list.글종류 === "추천")
-              .map((list) => <BookCards key={list.id} list={list} />)} */}
-
           {searchList.map((list) => {
             return list.글종류 === "중고거래" ? (
               <BookCards key={list.id} list={list} />
